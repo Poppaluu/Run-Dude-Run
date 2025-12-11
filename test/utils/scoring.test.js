515bdde764e21525b64ca0151de6a02c92b04b68
JUNU <m.l02n23@gmail.com>
2025-12-11 14:27:53 +0900

    feat (test):add test for storage.js

--- test/utils/scoring.test.js
+++ test/utils/scoring.test.js
@@ -1,7 +1,8 @@
 // test/utils/scoring.test.js
-import { test, expect } from '@jest/globals';
+import {test, expect, describe} from '@jest/globals';
 import { calculateScore } from '../../src/utils/scoring.js';
 
+describe('Game Score Tests', ()=> {})
 test('#1 Happy Path', () => {
     const result = calculateScore(
         100, // play_time
@@ -14,19 +15,6 @@ test('#1 Happy Path', () => {
 
     expect(result).toBe(243);
 });
-test('#2 Edge Case : When Input become too large', () => {
-    const result = calculateScore(
-        100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000, // play_time
-        5,   // hits
-        3,   // pickups
-        10,  // max_speed
-        15,  // max_jump_power
-        2    // health_left
-    );
-
-    expect(result).toBe( 2e+95); // should be the maximum value of Number Class
-});
-
 test('#2 Edge Case : When Input become too large', () => {
     const result = calculateScore(
         100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000, // play_time
